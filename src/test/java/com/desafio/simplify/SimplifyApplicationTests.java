8d031c35e070a9f7de5af7970e8be95102e20233
Pinheiro Robson <pinheirorobson7@gmail.com>
2024-10-17 18:46:07 -0300

    configuracao inicial do desafio

---
+++ src/test/java/com/desafio/simplify/SimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
